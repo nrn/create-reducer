4820f7bf8de453d6a26600ef5b7b01b57a30dd64
Nick Niemeir <nick.niemeir@gmail.com>
2015-11-21 14:09:13 -0800

    Update tests/docs.

--- readme.md
+++ readme.md
@@ -9,26 +9,27 @@ Big thanks to dlmanning, our conversations lead me here.
 var createReducer = require('create-reducer')
 
 // setup handlers, object keys for action types
-var handlers =
-  { add: add
-  , multiply: multiply
+var handlers = {
+  add: add,
+  multiply: multiply,
   // compose handler functions together for compound actions.
-  , addThenMultiply: createReducer.compose(add, multiply)
-  }
+  addThenMultiply: createReducer.compose(add, multiply)
+}
 
-// create a reducer from the handlers, this is normaly what would be exported.
-var reducer = createReducer(handlers)
+module.exports = createReducer(handlers, function () {
+  return { num: 0 }
+})
 
 // actual action handling functions, down out of the way.
 function add (state, action) {
   return {
-    num: state.num + action.num
+    num: state.num + action.payload
   }
 }
 
 function multiply (state, action) {
   return {
-    num: state.num * action.num
+    num: state.num * action.payload
   }
 }
 ```
--- test.js
+++ test.js
@@ -2,32 +2,39 @@ var createReducer = require('./index')
 var test = require('tape')
 
 // setup handlers, object keys for action types
-var handlers =
-  { add: add
-  , multiply: multiply
-  , createSubtract: createSubtract
+var handlers = {
+  add: add,
+  multiply: multiply,
+  createSubtract: createSubtract,
   // compose handler functions together for compound actions.
-  , addThenMultiply: createReducer.compose([add, multiply])
-  }
+  addThenMultiply: createReducer.compose([add, multiply])
+}
 
 // create a reducer from the handlers, this is normaly what would be exported.
-var reducer = createReducer(handlers, function () { return {num: 0}})
+var reducer = createReducer(handlers, function () {
+  return {
+    num: 0
+  }
+})
 
 // actual action handling functions, down out of the way.
 function add (state, action) {
-  state.num += action.num
-  return state
+  return {
+    num: state.num + action.payload
+  }
 }
 
 function multiply (state, action) {
-  state.num *= action.num
-  return state
+  return {
+    num: state.num * action.payload
+  }
 }
 
 function createSubtract (state) {
   this.subtract = function (state, action) {
-    state.num -= action.num
-    return state
+    return {
+      num: state.num - action.payload
+    }
   }
   return state
 }
@@ -36,16 +43,16 @@ function createSubtract (state) {
 test('use reducer', function (t) {
   var state = reducer(undefined, { type: 'n/a' })
   t.equal(state.num, 0, 'get initial state')
-  state = reducer(state, {type: 'add', num: 1})
+  state = reducer(state, {type: 'add', payload: 1})
   t.equal(state.num, 1, 'add')
-  state = reducer(state, {type: 'multiply', num: 2})
+  state = reducer(state, {type: 'multiply', payload: 2})
   t.equal(state.num, 2, 'multiply')
-  state = reducer(state, {type: 'addThenMultiply', num: 3})
+  state = reducer(state, {type: 'addThenMultiply', payload: 3})
   t.equal(state.num, 15, 'addThenMultiply')
-  state = reducer(state, {type: 'subtract', num: 4})
+  state = reducer(state, {type: 'subtract', payload: 4})
   t.equal(state.num, 15, 'Non-extant')
-  state = reducer(state, {type: 'createSubtract', num: 4})
-  state = reducer(state, {type: 'subtract', num: 4})
+  state = reducer(state, {type: 'createSubtract', payload: 4})
+  state = reducer(state, {type: 'subtract', payload: 4})
   t.equal(state.num, 11, 'created and used subtract')
   t.end()
 })
